93b50f840f9ead77f7abe2c04395f0243e489a7f
Matheus Ferraz <matheusb994@gmail.com>
2020-04-22 12:32:48 -0300

    Update test

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,9 +3,9 @@ var request = require('request');
 
 describe('Status and content', function () {
     describe('Main page', function () {
-        it('status', function (done) {
-            request('http://localhost:8080/', function (error, response, body) {
-                expect(response.statusCode).to.equal(200);
+        it('Main page content', function (done) {
+            request('http://localhost:8080', function (error, response, body) {
+                expect(__filename).to.equal('index.html');
                 done();
             });
         });
